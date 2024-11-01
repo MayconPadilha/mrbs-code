8fdb9785a5a56ebbc86b11e12bb7eff63acb7310
Maycon Padilha <maycon.jordan5000@gmail.com>
2024-11-01 14:06:23 -0300

    feat: adicao do arquivo de alteracao das variaveis default

--- docker_app/php/config.inc.php
+++ docker_app/php/config.inc.php
@@ -1,7 +1,9 @@
-<?php // -*-mode: PHP; coding:utf-8;-*-
+<?php
 namespace MRBS;
 
-$timezone = "Asia/Aden";
+use IntlDateFormatter;
+require_once 'lib/autoload.inc';
+
 $dbsys = "mysql";
 $db_host = "db";
 $db_database = "mrbs";
@@ -9,3 +11,41 @@ $db_login = "mrbs";
 $db_password = "mrbs";
 $db_tbl_prefix = "mrbs_";
 $db_persist = FALSE;
+
+/**************************************************************************
+ *   MRBS Configuration File
+ *   Configure this file for your site.
+ *   You shouldn't have to modify anything outside this file.
+ *
+ *   This file has already been populated with the minimum set of configuration
+ *   variables that you will need to change to get your system up and running.
+ *   If you want to change any of the other settings in systemdefaults.inc.php
+ *   or areadefaults.inc.php, then copy the relevant lines into this file
+ *   and edit them here.   This file will override the default settings and
+ *   when you upgrade to a new version of MRBS the config file is preserved.
+ *
+ *   NOTE: if you include or require other files from this file, for example
+ *   to store your database details in a separate location, then you should
+ *   use an absolute and not a relative pathname.
+ **************************************************************************/
+
+/**********
+ * Timezone
+ **********/
+$timezone = "America/Sao_Paulo";
+
+/*********************************
+* Site identification information
+*********************************/
+
+$mrbs_company = "Instituto Federal do Rio Grande do Sul Campus Restinga";
+
+/*********************************
+* TIMES SETTINGS
+*********************************/
+
+$morningstarts         = 7;
+$morningstarts_minutes = 30;
+
+$eveningends           = 22;
+$eveningends_minutes   = 30; 
